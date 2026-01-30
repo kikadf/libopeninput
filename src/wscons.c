5bbefece2f68bb7ddefd893833619ed359b52199
kikadf <kikadf.01@gmail.com>
2026-01-30 10:38:07 +0100

    Apply all local changes

--- src/wscons.c
+++ src/wscons.c
@@ -45,16 +45,97 @@ static void
 wscons_device_init_pointer_acceleration(struct wscons_device *device,
               struct motion_filter *filter);
 
-static int old_value = -1;
+
+static void
+debug_dump_seat_devices(struct libinput *libinput)
+{
+    struct libinput_device *device;
+	struct libinput_seat *seat;
+
+	seat = wscons_seat_get(libinput, default_seat, default_seat_name);
+
+    log_debug(libinput,
+              "KDBG: device list dump, seat=%s\n",
+              libinput_seat_get_logical_name(seat));
+
+    list_for_each(device, &seat->devices_list, link) {
+        log_debug(libinput,
+                  "   - dev=%p sysname=%s\n",
+                  device,
+                  libinput_device_get_sysname(device));
+    }
+}
+
+static void
+wscons_udev_handler(void *data)
+{
+	struct udev_input *input = data;
+	struct udev_device *udev_device;
+	struct libinput_seat *seat;
+	struct libinput_device *device;
+	uint64_t time;
+	struct timespec ts;
+	struct libinput_event *event;
+	const char *action, *devnode, *sysname;
+
+	udev_device = udev_monitor_receive_device(input->udev_monitor);
+	if (!udev_device)
+		return;
+
+	action = udev_device_get_action(udev_device);
+	devnode = udev_device_get_devnode(udev_device);
+	sysname = udev_device_get_sysname(udev_device);
+	if (!action || !devnode || !sysname)
+		goto out;
+	log_debug(&input->base, "KDBG, %s: %s %s %s\n", __func__,
+			action, devnode, sysname);
+
+	if (!(strneq(sysname, "wskbd", 5) || strneq(sysname, "wsmouse", 7)))
+		goto out;
+
+	if (streq(action, "add")) {
+		device = libinput_path_add_device(&input->base, devnode);
+		if (!device)
+			goto out;
+		log_debug(&input->base, "KDBG, %s: in add\n", __func__);
+		clock_gettime(CLOCK_REALTIME, &ts);
+		time = s2us(ts.tv_sec) + ns2us(ts.tv_nsec);
+		event = calloc(1, sizeof(*event));
+		post_device_event(device, time, LIBINPUT_EVENT_DEVICE_ADDED, event);
+	}
+	else if (streq(action, "remove")) {
+		seat = wscons_seat_get(&input->base, default_seat, default_seat_name);
+		log_debug(&input->base, "KDBG, %s: in remove\n", __func__);
+		list_for_each(device, &seat->devices_list, link) {
+			if (device->devname && strcmp(device->devname, devnode) == 0) {
+				clock_gettime(CLOCK_REALTIME, &ts);
+				time = s2us(ts.tv_sec) + ns2us(ts.tv_nsec);
+				event = calloc(1, sizeof(*event));
+				post_device_event(device, time, LIBINPUT_EVENT_DEVICE_REMOVED, event);
+				libinput_path_remove_device(device);
+				break;
+			}
+		}
+	}
+
+	debug_dump_seat_devices(&input->base);
+
+out:
+	udev_device_unref(udev_device);
+}
 
 static int
 udev_input_enable(struct libinput *libinput)
 {
-	struct libinput_seat *seat;
-	struct libinput_device *device;
+/*	struct libinput_seat *seat;
+	struct libinput_device *device;*/
+	struct udev_input *input = (struct udev_input *)libinput;
+	struct udev *udev = input->udev;
+	int fd;
 
-	seat = wscons_seat_get(libinput, default_seat, default_seat_name);
+/*	seat = wscons_seat_get(libinput, default_seat, default_seat_name);
 	list_for_each(device, &seat->devices_list, link) {
+		log_debug(&input->base, "KDBG, %s: in list: %s\n", __func__, device->devname);
 		device->fd = open_restricted(libinput, device->devname, O_RDWR);
 		device->source =
 		    libinput_add_fd(libinput, device->fd,
@@ -62,37 +143,85 @@ udev_input_enable(struct libinput *libinput)
 		if (!device->source) {
 			return -ENOMEM;
 		}
+	}*/
+
+	input->udev_monitor = udev_monitor_new_from_netlink(udev, "udev");
+	if (!input->udev_monitor) {
+		log_info(libinput, "udev: failed to create the udev monitor\n");
+		return -1;
 	}
+
+	if (udev_monitor_filter_add_match_subsystem_devtype(input->udev_monitor, "input", NULL)) {
+		log_info(libinput, "udev: failed to set up filter\n");
+		goto err;
+	}
+
+	if (udev_monitor_enable_receiving(input->udev_monitor)) {
+		log_info(libinput, "udev: failed to bind the udev monitor\n");
+		goto err;
+	}
+
+	fd = udev_monitor_get_fd(input->udev_monitor);
+	input->udev_monitor_source =
+			libinput_add_fd(&input->base, fd, wscons_udev_handler, input);
+	if (!input->udev_monitor_source) {
+		goto err;
+	}
+
 	return 0;
+
+err:
+	if (input->udev_monitor) {
+		udev_monitor_unref(input->udev_monitor);
+		input->udev_monitor = NULL;
+	}
+	return -1;
 }
 
 static void
 udev_input_disable(struct libinput *libinput)
 {
-	struct libinput_seat *seat;
-	struct libinput_device *device;
+/*	struct libinput_seat *seat;
+	struct libinput_device *device;*/
+	struct udev_input *input = (struct udev_input*)libinput;
 
-	seat = wscons_seat_get(libinput, default_seat, default_seat_name);
+	log_debug(&input->base, "KDBG, %s: disable cb\n", __func__);
+
+	if (input->udev_monitor_source) {
+		libinput_remove_source(&input->base, input->udev_monitor_source);
+		input->udev_monitor_source = NULL;
+	}
+	if (input->udev_monitor) {
+		udev_monitor_unref(input->udev_monitor);
+		input->udev_monitor = NULL;
+	}
+
+/*	seat = wscons_seat_get(libinput, default_seat, default_seat_name);
 	list_for_each(device, &seat->devices_list, link) {
 		if (device->source) {
 			libinput_remove_source(libinput, device->source);
 			device->source = NULL;
 		}
 		close_restricted(libinput, device->fd);
-	}
+	}*/
 }
 
 static void
 udev_input_destroy(struct libinput *libinput)
 {
-	struct libinput_seat *seat;
-	struct libinput_device *device;
+/*	struct libinput_seat *seat;
+	struct libinput_device *device;*/
+	struct udev_input *input = (struct udev_input*)libinput;
 
-	fprintf(stderr, "%s", __func__);
-	seat = wscons_seat_get(libinput, default_seat, default_seat_name);
+	if (input->udev) {
+		udev_unref(input->udev);
+		input->udev = NULL;
+	}
+
+/*	seat = wscons_seat_get(libinput, default_seat, default_seat_name);
 	list_for_each(device, &seat->devices_list, link) {
 		close_restricted(libinput, device->fd);
-	}
+	}*/
 }
 
 static int
@@ -128,13 +257,13 @@ wscons_process(struct libinput_device *device, struct wscons_event *wsevent)
 		key = wsevent->value;
 		if (wsevent->type == WSCONS_EVENT_KEY_UP) {
 			kstate = LIBINPUT_KEY_STATE_RELEASED;
-			old_value = -1;
+			dev->old_value = -1;
 		} else {
 			kstate = LIBINPUT_KEY_STATE_PRESSED;
 			/* ignore auto-repeat */
-			if (key == old_value)
+			if (key == dev->old_value)
 				return;
-			old_value = key;
+			dev->old_value = key;
 		}
 		keyboard_notify_key(device, time,
 		    wskey_transcode(wscons_device(device)->scanCodeMap, key), kstate);
@@ -239,7 +368,7 @@ wscons_device_dispatch(void *data)
 	ssize_t len;
 	int count, i;
 
-	len = read(device->fd, wsevents, sizeof(struct wscons_event));
+	len = read(device->fd, wsevents, sizeof(wsevents));
 	if (len <= 0 || (len % sizeof(struct wscons_event)) != 0)
 		return;
 
@@ -282,20 +411,27 @@ wscons_seat_get(struct libinput *libinput, const char *seat_name_physical,
 
 LIBINPUT_EXPORT struct libinput *
 libinput_udev_create_context(const struct libinput_interface *interface,
-			     void *user_data,
-			     struct udev *udev)
+				 void *user_data,
+				 struct udev *udev)
 {
-	struct libinput *libinput;
+	struct udev_input *input;
 
-	libinput = calloc(1, sizeof(*libinput));
-	if (libinput == NULL)
+	if (!interface || !udev)
 		return NULL;
 
-	if (libinput_init(libinput, interface, &interface_backend, user_data) != 0) {
-		free(libinput);
+	input = calloc(1, sizeof(*input));
+	if (!input)
+		return NULL;
+
+	if (libinput_init(&input->base, interface, &interface_backend, user_data) != 0) {
+		libinput_unref(&input->base);
+		free(input);
 		return NULL;
 	}
-	return libinput;
+
+	input->udev = udev_ref(udev);
+
+	return &input->base;
 }
 
 LIBINPUT_EXPORT int
@@ -307,6 +443,7 @@ libinput_udev_assign_seat(struct libinput *libinput, const char *seat_id)
 	uint64_t time;
 	struct timespec ts;
 	struct libinput_event *event;
+	struct udev_input *input = (struct udev_input*)libinput;
 
 	/* Add standard devices */
 	for (int i = 0; i < 10; i++) {
@@ -332,6 +469,11 @@ libinput_udev_assign_seat(struct libinput *libinput, const char *seat_id)
 		post_device_event(device, time, LIBINPUT_EVENT_DEVICE_ADDED,
 		    event);
 	}
+	log_debug(&input->base, "KDBG, %s\n", __func__);
+
+	if (udev_input_enable(&input->base) < 0)
+		return -1;
+
 	return 0;
 }
 
@@ -517,6 +659,8 @@ wscons_device_init(struct wscons_device *wscons_device)
 {
 	struct libinput_device *device = &wscons_device->base;
 
+	wscons_device->old_value = -1;
+
 	if (strncmp(device->devname, "/dev/wsmouse", 12) == 0) {
 		/* XXX handle tablets and touchpanel */
 		wscons_device->capability = LIBINPUT_DEVICE_CAP_POINTER;
@@ -576,8 +720,18 @@ libinput_path_add_device(struct libinput *libinput,
 	return device;
 
 err:
-	if (device != NULL)
-		close_restricted(libinput, device->fd);
+	if (device != NULL) {
+		if (device->source)
+			libinput_remove_source(libinput, device->source);
+		if (device->devname) {
+			free(device->devname);
+			device->devname = NULL;
+		}
+		if (device->fd >= 0)
+			close_restricted(libinput, device->fd);
+	} else if (fd >= 0) {
+		close_restricted(libinput, fd);
+	}
 	free(wscons_device);
 	return NULL;
 }
@@ -587,6 +741,7 @@ libinput_path_remove_device(struct libinput_device *device)
 {
 	struct libinput *libinput = device->seat->libinput;
 
+	list_remove(&device->link);
 	libinput_remove_source(libinput, device->source);
 	device->source = NULL;
 
--- src/wscons.h
+++ src/wscons.h
@@ -18,12 +18,19 @@ struct wscons_device {
 	struct libinput_device base;
 	enum libinput_device_capability capability;
 	struct TransMapRec *scanCodeMap;
+	int old_value;
 	struct {
 		struct libinput_device_config_accel config;
 		struct motion_filter *filter;
 	} pointer;
 };
 
+struct udev_input {
+	struct libinput base;
+	struct udev *udev;
+	struct udev_monitor *udev_monitor;
+	struct libinput_source *udev_monitor_source;
+};
 
 static inline struct wscons_device *
 wscons_device(struct libinput_device *device)
